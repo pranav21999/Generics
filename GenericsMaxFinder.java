417faa31cc12fda145b25bbed0c5d46eebaa9e79
pranav21999 <pranavdani99@gmail.com>
2021-06-28 21:22:20 +0530

    method to find max of Three String is added

--- GenericsMaxFinder.java
+++ GenericsMaxFinder.java
@@ -1,16 +1,19 @@
 package generics;
 
 public class GenericsMaxFinder {
-	//main method
+	//main  method
 	public static void main(String[] args) 
 	{
 		Integer a=2,b=7,c=5;
 		Float p=4.f,q=2.3f,r=9.8f;
+		String x="abc",y="aaddf",z="qertgh";
 		findMaxInt(a,b,c); 
 		findMaxFloat(p,q,r); 
+		findMaxString(x,y,z); 
+		
+		
 	}
-
-	//method to find max of  Integer
+	//max of three Int
 	public static void findMaxInt(Integer a,Integer b,Integer c)
 	{
 		Integer max=a;
@@ -18,9 +21,9 @@ public class GenericsMaxFinder {
 			max=b;
 		else if(c.compareTo(max)>0)
 			max=c;
-		System.out.printf("maximum of numbers: [%s ,%s, %s]is %s ",a,b,c,max);
+		System.out.printf("maximum of numbers: [%s ,%s, %s]is %s ",a,b,c,max);			
 	}
-	//method to find maximum of three float
+	//max of three Float
 	public static void findMaxFloat(Float p,Float q,Float r)
 	{
 		Float max=p;
@@ -30,8 +33,21 @@ public class GenericsMaxFinder {
 		 else if (r.compareTo(max)>0) {
 	            max = r;
 	        }
-	        System.out.printf("Max value of %s %s %s is : %s\n", p,q,r, max);
+	        System.out.printf("Max value of %s %s %s is : %s\n", p,q,r, max);			
 	}
+	//max of three String
+	public static void findMaxString(String x,String y,String z)
+	{
+		String max=x;
+		 if (y.compareTo(max)>0) {
+	            max = y;
+	        }
+		 else if (z.compareTo(max)>0) {
+	            max = z;
+	        }
+	        System.out.printf("Max value of %s %s %s is : %s\n", x,y,z ,max);			
+	}
+
 
 
 }
