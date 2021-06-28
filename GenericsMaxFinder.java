aad6d669bf477a9a67d31bdb3561bac7b501ca27
pranav21999 <pranavdani99@gmail.com>
2021-06-28 21:46:46 +0530

    Generics method to find max of any Data type is added

--- GenericsMaxFinder.java
+++ GenericsMaxFinder.java
@@ -1,53 +1,21 @@
 package generics;
 
 public class GenericsMaxFinder {
-	//main  method
-	public static void main(String[] args) 
-	{
-		Integer a=2,b=7,c=5;
-		Float p=4.f,q=2.3f,r=9.8f;
-		String x="abc",y="aaddf",z="qertgh";
-		findMaxInt(a,b,c); 
-		findMaxFloat(p,q,r); 
-		findMaxString(x,y,z); 
-		
-		
-	}
-	//max of three Int
-	public static void findMaxInt(Integer a,Integer b,Integer c)
-	{
-		Integer max=a;
-		if(b.compareTo(max)>0)
-			max=b;
-		else if(c.compareTo(max)>0)
-			max=c;
-		System.out.printf("maximum of numbers: [%s ,%s, %s]is %s ",a,b,c,max);			
-	}
-	//max of three Float
-	public static void findMaxFloat(Float p,Float q,Float r)
-	{
-		Float max=p;
-		 if (q.compareTo(max)>0) {
-	            max = q;
-	        }
-		 else if (r.compareTo(max)>0) {
-	            max = r;
-	        }
-	        System.out.printf("Max value of %s %s %s is : %s\n", p,q,r, max);			
-	}
-	//max of three String
-	public static void findMaxString(String x,String y,String z)
-	{
-		String max=x;
-		 if (y.compareTo(max)>0) {
-	            max = y;
-	        }
-		 else if (z.compareTo(max)>0) {
-	            max = z;
-	        }
-	        System.out.printf("Max value of %s %s %s is : %s\n", x,y,z ,max);			
-	}
+    // Main method
+    public static void main(String[] args) {
 
+        System.out.println("Maximum Value of Integer given is : " + GenericMax(6, 3, 9));//max of Int
+        System.out.println("Maximum value of Float numbers given is : " + GenericMax(1.0f, 5.0f, 3.0f));//max of float
+        System.out.println("Maximum value of String given is =: " + GenericMax("Apple", "Peach", "Banana"));//max of String
+         }
 
-
+    // Generic method to find max of any data type using compareTo method
+    public static <T extends Comparable<T>> T GenericMax(T num1, T num2, T num3) {
+        T max = num1;
+        if (num2.compareTo(max) > 0)
+            max = num2;
+        if (num3.compareTo(max) > 0)
+            max = num3;
+        return max;
+    }
 }
